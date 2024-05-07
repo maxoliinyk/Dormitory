81a147209ca9b31f2cd3d4664cb982b6942720fc
maxoliinyk <i4m.oliinyk@gmail.com>
2024-05-07 18:54:58 +0300

    Initial Commit

---
+++ Dormitory/DormitoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormitoryApp.swift
+//  Dormitory
+//
+//  Created by Max Oliinyk on 07.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct DormitoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
