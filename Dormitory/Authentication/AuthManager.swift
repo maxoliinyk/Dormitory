6a49809ca25dfcf4f95053d295b3f7307b3691b6
maxoliinyk <i4m.oliinyk@gmail.com>
2024-05-20 12:42:44 +0300

    Add email authentication
    Functions as intended; UI not polished yet

---
+++ Dormitory/Authentication/AuthManager.swift
@@ -0,0 +1,55 @@
+//
+//  AuthManager.swift
+//  Dormitory
+//
+//  Created by Max Oliinyk on 16.05.2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+struct AuthDataResultModel {
+    let uid: String
+    let email: String?
+    let photoURL: String?
+    
+    init(user: User) {
+        self.uid = user.uid
+        self.email = user.email
+        self.photoURL = user.photoURL?.absoluteString
+    }
+}
+
+final class AuthManager {
+    
+    static let shared = AuthManager()
+    private init() {  }
+    
+    func getAuthenticatedUser() throws -> AuthDataResultModel {
+        guard let user = Auth.auth().currentUser else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return AuthDataResultModel(user: user)
+    }
+    
+    @discardableResult
+    func createUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    func signInUser(email: String, password: String) async throws -> AuthDataResultModel {
+        let authDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
+        return AuthDataResultModel(user: authDataResult.user)
+    }
+    
+    func resetPassword(email: String) async throws {
+        try await Auth.auth().sendPasswordReset(withEmail: email)
+    }
+    
+    func signOut() throws {
+        try Auth.auth().signOut()
+    }
+    
+}
