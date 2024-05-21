1e36881ad5751b0eaa78978968126921502954f0
maxoliinyk <i4m.oliinyk@gmail.com>
2024-05-21 17:06:04 +0300

    Placeholder for Google Sign In

---
+++ Dormitory/Core/Authentication/AuthenticationViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationViewModel.swift
+//  Dormitory
+//
+//  Created by Max Oliinyk on 21.05.2024.
+//
+
+import Foundation
+
+@MainActor
+final class AuthenticationViewModel {
+    
+    func signInGoogle() async throws {
+        
+    }
+}
