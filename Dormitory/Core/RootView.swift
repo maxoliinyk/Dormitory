559571e56c0b53e705cb4e52832cfe8a5467df93
maxoliinyk <i4m.oliinyk@gmail.com>
2024-05-24 20:39:32 +0300

    Separate Admin and User views

--- Dormitory/Core/RootView.swift
+++ Dormitory/Core/RootView.swift
@@ -10,22 +10,47 @@ import SwiftUI
 struct RootView: View {
     
     @State private var showSignUpView = false
+    @State private var isAdmin = false
+    @State private var isLoading = true // Added loading state
     
     var body: some View {
         ZStack {
             NavigationStack {
-                ProfileView(showSignUpView: $showSignUpView)
+//                DormitoriesView()
+                if isLoading {
+                    ProgressView("Loading...") // Show a loading indicator while fetching user data
+                } else {
+                    if isAdmin {
+                        AdminNotificationView(showSignUpView: $showSignUpView)
+                    } else {
+                        UserNotificationView(showSignUpView: $showSignUpView)
+                    }
+                }
+//                ProfileView(showSignUpView: $showSignUpView)
             }
         }
         .onAppear() {
-            let authUser = try? AuthManager.shared.getAuthenticatedUser()
-            self.showSignUpView = authUser == nil
+            Task {
+                await loadUserData()
+            }
         }
         .fullScreenCover(isPresented: $showSignUpView) {
             NavigationStack {
                 AuthView(showSignUpView: $showSignUpView)
             }
         }
+    }
+    
+    private func loadUserData() async {
+        do {
+            let authUser = try AuthManager.shared.getAuthenticatedUser()
+            let dbUser = try await UserManager.shared.getUser(userID: authUser.uid)
+            self.isAdmin = dbUser.isAdmin
+        } catch {
+            print("Error fetching user data: \(error.localizedDescription)")
+            self.showSignUpView = true
+        }
+        self.isLoading = false // Set loading state to false after fetching user data
     }
 }
 
