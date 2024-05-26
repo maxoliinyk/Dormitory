7102be91d63e4e4c46d1ab13c809731a55eaa556
maxoliinyk <i4m.oliinyk@gmail.com>
2024-05-26 14:59:20 +0300

    Better Firestore compatibility

--- Dormitory/Firestore/RequestManager.swift
+++ Dormitory/Firestore/RequestManager.swift
@@ -6,3 +6,85 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+
+struct DBRequest: Codable, Equatable, Identifiable {
+    let requestID: String
+    let dormitoryID: String
+    let title: String
+    let content: String
+    let postedBy: String
+    let roomNumber: String
+    let date: Timestamp
+    
+    var id: String {
+        return requestID
+    }
+}
+
+final class RequestManager {
+
+    static let shared = RequestManager()
+    private init() { }
+
+    private let requestCollection = Firestore.firestore().collection("requests")
+
+    private func requestDocument(id: String) -> DocumentReference {
+        requestCollection.document(id)
+    }
+
+    func uploadRequest(request: DBRequest) async throws {
+        try await requestCollection.addDocument(from: request)
+    }
+    
+    func getRequest(id: String) async throws -> DBRequest {
+        try await requestDocument(id: id).getDocument(as: DBRequest.self)
+    }
+
+    func fetchAllRequests() async throws -> [DBRequest] {
+        var requests: [DBRequest] = []
+        
+        do {
+            let querySnapshot = try await requestCollection.getDocuments()
+            requests = try querySnapshot.documents.compactMap { document in
+                try document.data(as: DBRequest.self)
+            }
+        } catch {
+            print("Error fetching requests: \(error)")
+            throw error
+        }
+        
+        return requests
+    }
+    
+    func deleteRequest(requestID: String) async throws {
+        try await requestDocument(id: requestID).delete()
+    }
+
+    // New function to fetch requests for a specific dormitory ID
+    func getRequests(forDormitoryID dormitoryID: String) async throws -> [DBRequest] {
+        var requests: [DBRequest] = []
+        
+        do {
+            let querySnapshot = try await requestCollection.whereField("dormitoryID", isEqualTo: dormitoryID).getDocuments()
+            requests = try querySnapshot.documents.compactMap { document in
+                try document.data(as: DBRequest.self)
+            }
+        } catch {
+            print("Error fetching requests for dormitory ID \(dormitoryID): \(error)")
+            throw error
+        }
+        
+        return requests
+    }
+
+    private func convertDateStringToTimestamp(dateString: String) -> Timestamp? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        if let date = formatter.date(from: dateString) {
+            return Timestamp(date: date)
+        }
+        return nil
+    }
+}
--- Dormitory/Firestore/UserManager.swift
+++ Dormitory/Firestore/UserManager.swift
@@ -17,6 +17,48 @@ struct DBUser: Codable {
     let roomNumber: String?
     let dormitoryID: String
     let isAdmin: Bool
+
+    init(userID: String, email: String? = nil, name: String? = nil, lastName: String? = nil, roomNumber: String? = nil, dormitoryID: String, isAdmin: Bool) {
+        self.userID = userID
+        self.email = email
+        self.name = name
+        self.lastName = lastName
+        self.roomNumber = roomNumber
+        self.dormitoryID = dormitoryID
+        self.isAdmin = isAdmin
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case userID
+        case email
+        case name
+        case lastName
+        case roomNumber
+        case dormitoryID
+        case isAdmin
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        userID = try container.decode(String.self, forKey: .userID)
+        email = try? container.decode(String?.self, forKey: .email) ?? ""
+        name = try? container.decode(String?.self, forKey: .name) ?? ""
+        lastName = try? container.decode(String?.self, forKey: .lastName) ?? ""
+        roomNumber = try? container.decode(String?.self, forKey: .roomNumber) ?? ""
+        dormitoryID = try container.decode(String.self, forKey: .dormitoryID)
+        isAdmin = try container.decode(Bool.self, forKey: .isAdmin)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(userID, forKey: .userID)
+        try container.encode(email, forKey: .email)
+        try container.encode(name, forKey: .name)
+        try container.encode(lastName, forKey: .lastName)
+        try container.encode(roomNumber, forKey: .roomNumber)
+        try container.encode(dormitoryID, forKey: .dormitoryID)
+        try container.encode(isAdmin, forKey: .isAdmin)
+    }
 }
 
 final class UserManager {
